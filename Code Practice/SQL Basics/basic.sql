f594ac68fbefde6804812921f4f724784813f8d5
pokhraj prasad <pokhraj18@gmail.com>
2025-02-03 13:31:19 +0530

    sql basics added

---
+++ Code Practice/SQL Basics/basic.sql
@@ -0,0 +1,46 @@
+-- create database space
+-- use space
+
+-- create table employee
+-- (
+-- id int, 
+-- name varchar(255), 
+-- age int, 
+-- gender varchar(255), 
+-- salary int, 
+-- department varchar(255), 
+-- city varchar(255)
+-- )
+
+--drop table employee
+
+-- select * from employee
+
+-- insert into employee VALUES
+-- (1, 'Anuj', 32, 'Male', 40000, 'It', 'Delhi')
+
+-- insert into employee VALUES
+-- (2, 'Anjali', 24, 'Female', 45000, 'HR', 'Dehradun'),
+-- (3, 'Rajat', 30, 'Male', 35000, 'Accounts', 'Jaipur'),
+-- (4, 'Riya', 27, 'Female', 55000, 'It', 'Chennai'),
+-- (5, 'Rahul', 32, 'Male', 45000, 'HR', 'Pune'),
+-- (6, 'Gopal', 30, 'Male', 60000, 'It', 'Bhopal'),
+-- (7, 'Vini', 27, 'Female', 44000, 'Accounts', 'Chennai'),
+-- (8, 'Badal', 40, 'Male', 59000, 'It', 'Kochi')
+
+-- select id, name, age  from employee
+
+-- insert into  employee VALUES
+-- (9, 'Abhay', 39, 'Male', 57000, 'Marketing', 'Bhopal')
+
+-- select distinct * from employee
+
+-- select top(2) * from employee
+
+-- select top(3) * from employee
+
+-- select * from employee where gender = 'Male'
+
+-- select * from employee where department = 'HR'
+
+-- select * from employee where id = 7
