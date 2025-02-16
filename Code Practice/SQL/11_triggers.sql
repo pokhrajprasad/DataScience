c79b9bb60d8d0492d3f39763d81ef2aa6b64980f
pokhraj prasad <pokhraj18@gmail.com>
2025-02-16 11:16:09 +0530

    Updated till transaction

---
+++ Code Practice/SQL/11_triggers.sql
@@ -0,0 +1,29 @@
+-- use space
+
+-- select * from employee
+
+-- create trigger Trig_up_A on employee after update -- after insert,delete
+-- as BEGIN
+-- print 'your data has been updated'
+-- end;
+
+-- update employee set salary = 75000 where id = 19
+
+-- create trigger Trig_in on employee instead of INSERT
+-- as begin
+-- print 'you cannot insert any data in the table'
+-- END
+
+-- insert into employee values(20,'Neha',36,'Female',56000,'Accounts','Oslo','Norway')
+
+-- select * from employee
+
+-- SELECT 
+--     name AS TriggerName
+-- FROM sys.triggers;
+
+-- Drop trigger Trig_up_A
+-- Drop trigger Trig_in
+
+
+
