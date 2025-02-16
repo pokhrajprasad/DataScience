837c5f8efd9bce7b562be8b15f217395b074b3f1
pokhraj prasad <pokhraj18@gmail.com>
2025-02-16 11:39:23 +0530

    Updated till exception handling

---
+++ Code Practice/SQL/13_exception_handling.sql
@@ -0,0 +1,27 @@
+use space
+
+select * from employee
+
+begin transaction Salary_Employee
+begin TRY
+select salary/0 from employee
+end TRY
+begin CATCH
+select ERROR_MESSAGE() as ERROR_MESSAGE
+end catch
+
+begin transaction salary_123
+begin TRY
+select salary from employee
+end TRY
+begin CATCH
+select ERROR_MESSAGE() as ERROR_MESSAGE
+end catch
+
+begin transaction Salary_321
+begin TRY
+select*,select* name as newly_weekly from employee
+end TRY
+begin CATCH
+select ERROR_MESSAGE() as ERROR_MESSAGE
+end catch
