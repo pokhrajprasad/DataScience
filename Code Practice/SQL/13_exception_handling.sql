951cffc7d3bfec21f51b49364e85c2888bd03fdc
pokhraj prasad <pokhraj18@gmail.com>
2025-02-16 11:42:19 +0530

    files updated

--- Code Practice/SQL/13_exception_handling.sql
+++ Code Practice/SQL/13_exception_handling.sql
@@ -1,22 +1,22 @@
-use space
+-- use space
 
-select * from employee
+-- select * from employee
 
-begin transaction Salary_Employee
-begin TRY
-select salary/0 from employee
-end TRY
-begin CATCH
-select ERROR_MESSAGE() as ERROR_MESSAGE
-end catch
+-- begin transaction Salary_Employee
+-- begin TRY
+-- select salary/0 from employee
+-- end TRY
+-- begin CATCH
+-- select ERROR_MESSAGE() as ERROR_MESSAGE
+-- end catch
 
-begin transaction salary_123
-begin TRY
-select salary from employee
-end TRY
-begin CATCH
-select ERROR_MESSAGE() as ERROR_MESSAGE
-end catch
+-- begin transaction salary_123
+-- begin TRY
+-- select salary from employee
+-- end TRY
+-- begin CATCH
+-- select ERROR_MESSAGE() as ERROR_MESSAGE
+-- end catch
 
 begin transaction Salary_321
 begin TRY
