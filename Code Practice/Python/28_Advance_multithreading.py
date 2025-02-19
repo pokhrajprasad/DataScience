ab9e67497dee67f6c29a765b92dceea03e8cce42
pokhraj prasad <pokhraj18@gmail.com>
2025-02-19 14:24:36 +0530

    Updated till multiprocessing

---
+++ Code Practice/Python/28_Advance_multithreading.py
@@ -0,0 +1,17 @@
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+def print_number(number):
+    time.sleep(1)
+    return f"number :{number+1}"
+
+numbers=[1,2,3,4,5]
+
+with ThreadPoolExecutor(max_workers=3) as executor:
+    results=executor.map(print_number,numbers)
+
+# for result in results:
+#     print(result)
+
+print(list(results))
+print(type(results))
